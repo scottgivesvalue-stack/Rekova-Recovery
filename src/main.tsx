0579569f35e119ac142b697a997ac2e53a865bdd
Scott Vasilisin <scottgivesvalue@Scotts-MacBook-Air.local>
2026-07-16 13:42:04 -0400

    Remove redirect thrash causing page flicker

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
-const params = new URLSearchParams(window.location.search);
-const appMode = params.get('app') === '1';
-const rootPath = window.location.pathname === '/' || window.location.pathname === '/Rekova-Recovery/';
-let shouldRenderApp = true;
-
-if (rootPath && !appMode) {
-  const landingPath = window.location.pathname.endsWith('/Rekova-Recovery/')
-    ? '/Rekova-Recovery/landing'
-    : '/landing';
-  window.location.replace(landingPath);
-  shouldRenderApp = false;
-}
-
-if (shouldRenderApp) {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
